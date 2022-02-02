dc460131ecba1522f284d5f158decdd01291c2ec
Naresh.Vaka <Naresh.Vaka@BHAVNAWKS724.BHAVNACORP.NET>
2022-02-02 08:04:00 +0530

    committed successfully

--- ZipService/src/com/bhavna/zipservice1/ZipService1.java
+++ ZipService/src/com/bhavna/zipservice1/ZipService1.java
@@ -16,6 +16,7 @@ package com.bhavna.zipservice1;
 public abstract class ZipService1 // abstract class for zipservice1...
 {
 	// abstract methods...
+	
 	abstract public void printDetails();
 
 	abstract public void getByPinCode(int pinCode);
